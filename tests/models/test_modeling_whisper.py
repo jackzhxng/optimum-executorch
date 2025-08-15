11ed885495d2e551f0c7a494e445038de900a8cc
Jack Zhang <32371937+jackzhxng@users.noreply.github.com>
2025-08-15 11:03:24 -0700

    Re-enable whisper tests

--- tests/models/test_modeling_whisper.py
+++ tests/models/test_modeling_whisper.py
@@ -34,10 +34,6 @@ from optimum.utils.import_utils import is_transformers_version
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
-@pytest.mark.skipif(
-    is_transformers_version(">", "4.52.4"),
-    reason="Need to fix in the transformers due to attention refactor https://github.com/huggingface/transformers/pull/38235",
-)
 class ExecuTorchModelIntegrationTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
